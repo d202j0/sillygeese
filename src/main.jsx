db30450d7695b61e988c73c2180a531249b9bcdf
Tom Wilcock <tomwilcock@gmail.com>
2026-03-20 21:42:00 -0400

    add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { inject } from '@vercel/analytics'
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
