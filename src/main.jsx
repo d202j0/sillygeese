08900fc7dabcc21e8f6fe8e09b39bf396157b410
Tom Wilcock <tomwilcock@gmail.com>
2026-03-21 22:56:04 -0400

    fix vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics'
-inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
